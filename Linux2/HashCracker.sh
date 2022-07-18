343c36da08e270d39336b818a1536089bad822ee
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-17 22:47:13 -0500

    commit

--- Linux2/HashCracker.sh
+++ Linux2/HashCracker.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-
 # storing the hash in a variable for easy access
-myHash='14a4b8e7ae966e72a3a2c51633bfabc6'
+MySampleHash='14a4b8e7ae966e72a3a2c51633bfabc6'
 
-# using for loop to go over metasploit password list - other wordlists could be utilized if desired. 
-for word in $(cat '/usr/share/metasploit-framework/data/wordlists/password.lst')      
-    do
-    wordHash= printf \$word | md5sum | cut -d " " -f 1\
-	    if [ "$wordHash" =="$myHash" ]
-	    then
-		    printf "==========================%s\n"
-		    printf "Found Password: $word%s\n"
-		    printf "==========================%s\n"
-		    break
-	else
-        printf "Attempted Password: $word\n"
-        printf "Sample Hash: $myHash\n"
-		printf "Attempted Hash: $wordHash"
+# using for loop to go over metasploit password list 
+for word in $(cat /usr/share/metasploit-framework/data/wordlists/password.lst)
+do
+		
+	if [ '$word | md5sum' == '$MySampleHash' ]
+
+		 then
+			printf "==========================\n"
+			printf "Found Password: $word\n"
+			printf "==========================\n"
+			sleep 30
+			break
+		 else
+			printf "Password: $word\n"
+			printf "Sample Hash: $MySampleHash\n"
+			printf "Current Hash:\r"
+		        printf $word | md5sum\n
 	fi
 done
