efcc445d2af26612ba8e12c47ebcb5204c91cbde
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-18 15:45:35 -0500

    commit

--- Linux2/HashCrackerMidTerm.sh
+++ Linux2/HashCrackerMidTerm.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # storing the hash in a variable for easy access
 gmMySampleHash='ba49ec1e5a09c8f4edee0ee05463426fa0dd9c285bc678a614d54e171d85d752'
-gmAnswer=$("$gmMySampleHash" | sha256sum)
-printf $gmAnswer | sha256sum| cut -d " " -f 1
+
 # using for loop to go over metasploit password list 
 for word in $(cat /usr/share/metasploit-framework/data/wordlists/password.lst)
 do		
-	if [ $(printf $word | md5sum| cut -d " " -f 1) == $MySampleHash ]
+	if [ $(printf $word | sha256sum| cut -d " " -f 1) == $gmMySampleHash ]
 		 then
 			printf "==========================\n"
 			printf "Found Password & Hash^: $word\n"
@@ -14,7 +13,7 @@ do
 			break
 		 else
 			printf "Trying Password: $word\n"
-			printf "Seeking Hash: $mySampleHash\n"
+			printf "Seeking Hash: $gmMySampleHash\n"
 			printf $word | md5sum| cut -d " " -f 1
 	fi
 done
