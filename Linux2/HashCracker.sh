5bf18cfcc0e5fda3e1d6e46aa0f280532075f803
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-17 20:35:10 -0500

    commit

--- Linux2/HashCracker.sh
+++ Linux2/HashCracker.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # storing the hash in a variable for easy access
-hash='14a4b8e7ae966e72a3a2c51633bfabc6'
+myHash='14a4b8e7ae966e72a3a2c51633bfabc6'
 
 # using for loop to go over metasploit password list - other wordlists could be utilized if desired. 
-for word in $(cat /usr/share/metasploit-framework/data/wordlists/password.lst)
-do      
-        
-	        if [ printf $word | md5| cut -d “ “ -f 1 == $hash ]
-                then 
-                        printf "==========================\n"
-                        printf "Found Password: $word\n"
-                        printf "==========================\n"
-                        break
-                else
-                        printf "Password: $word\n"
-                        printf "Sample Hash: $hash\n"
-                        printf "Current Hash: $word\n"
-        fi
+for word in $(cat /usr/share/metasploit-framework/data/wordlists/password.lst)      
+do
+
+wordHash= printf \$word | md5sum | cut -d " " -f 1\
+	
+		if [ "$wordHash" =="$myHash" ]
+		 then	
+			printf "==========================%s\n"
+			printf "Found Password: $word%s\n"
+			printf "==========================%s\n"
+			break
+		 else
+        		printf "Attempted Password: $word\n"
+                	printf "Sample Hash: $myHash\n"
+			printf "Attempted Hash: $wordHash"
+	fi
 done
