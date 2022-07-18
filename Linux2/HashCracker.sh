c426c8b9fa39b79ab2c44ed58bc479b6ca5f1b18
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-17 23:54:17 -0500

    HashCracker Commit

--- Linux2/HashCracker.sh
+++ Linux2/HashCracker.sh
@@ -2,7 +2,7 @@
 # storing the hash in a variable for easy access
 MySampleHash='14a4b8e7ae966e72a3a2c51633bfabc6'
 word="blyvooruitsig"
-printf $word | md5sum | cut -d " " -f 1
+printf $word | md5sum| cut -d " " -f 1
 # using for loop to go over metasploit password list 
 for word in $(cat /usr/share/metasploit-framework/data/wordlists/password.lst)
 do		
