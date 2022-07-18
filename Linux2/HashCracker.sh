3b8ba6cac2bbfc40f08bfbd64a16541320273112
George Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 20:45:07 -0500

    Update HashCracker.sh

--- Linux2/HashCracker.sh
+++ Linux2/HashCracker.sh
@@ -7,15 +7,15 @@ myHash='14a4b8e7ae966e72a3a2c51633bfabc6'
 for word in $(cat '/usr/share/metasploit-framework/data/wordlists/password.lst')      
     do
     wordHash= printf \$word | md5sum | cut -d " " -f 1\
-		if [ "$wordHash" =="$myHash" ]
-		 then	
-			printf "==========================%s\n"
-			printf "Found Password: $word%s\n"
-			printf "==========================%s\n"
-			break
-		 else
-        	printf "Attempted Password: $word\n"
-            printf "Sample Hash: $myHash\n"
-			printf "Attempted Hash: $wordHash"
-	    fi
+	    if [ "$wordHash" =="$myHash" ]
+	    then
+		    printf "==========================%s\n"
+		    printf "Found Password: $word%s\n"
+		    printf "==========================%s\n"
+		    break
+	else
+        printf "Attempted Password: $word\n"
+        printf "Sample Hash: $myHash\n"
+		printf "Attempted Hash: $wordHash"
+	fi
 done
