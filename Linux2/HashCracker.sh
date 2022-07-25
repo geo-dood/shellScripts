b73bf5d0ff3979eea8cb0397a5662f2480d60abc
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-25 05:22:09 -0500

    First Script week 5 push

--- Linux2/HashCracker.sh
+++ Linux2/HashCracker.sh
@@ -5,9 +5,9 @@ myHash='14a4b8e7ae966e72a3a2c51633bfabc6'
 
 # using for loop to go over metasploit password list - other wordlists could be utilized if desired. 
 for word in $(cat '/usr/share/metasploit-framework/data/wordlists/password.lst')      
-    do
-    wordHash= printf \$word | md5sum | cut -d " " -f 1\
-	    if [ "$wordHash" =="$myHash" ]
+do
+
+	if [ "$wordHash" =="$myHash" ]
 	    then
 		    printf "==========================%s\n"
 		    printf "Found Password: $word%s\n"
